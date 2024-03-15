f6f726ce0397efc9fa4587e0a67568524361e2f1
Denys <oranjevo@gmail.com>
2024-03-16 00:26:03 +0800

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const knex = require('knex');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-const PORT = process.env.DATABASE_URL;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
@@ -15,9 +15,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    user: 'postgres',
+    password: 'new',
+    database: 'smart-brain',
   },
 });
 const register = require('./controllers/register');
@@ -28,7 +28,7 @@ const image = require('./controllers/image');
 const saltRounds = 10; // for bcrypt
 
 app.get('/', (req, res) => {
-  res.send('it is working');
+  res.send(db.users);
 });
 app.post('/signin', signin.handleSignIn(db, bcrypt));
 app.post('/register', (req, res) => {
