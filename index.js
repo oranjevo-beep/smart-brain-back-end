fb1558c1949ebd6bcd8e12355b3fb59457ced96b
Denys <oranjevo@gmail.com>
2024-03-16 13:28:38 +0800

    commit

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 const db = knex({
   client: 'pg',
   connection: {
