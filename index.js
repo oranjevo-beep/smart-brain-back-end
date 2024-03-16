892124ab8a31b166ce8d2527f2edc3627f712ae0
Denys <oranjevo@gmail.com>
2024-03-16 12:13:53 +0800

    commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
